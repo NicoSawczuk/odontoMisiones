1c0dd3ff7d2bd955e3a56976ed132d443f76690b
Nicolas Sawczuk <nico.290698@hotmail.com>
2020-06-30 15:34:16 -0300

    Links a sidebar

--- resources/views/admin-lte/sidebar.blade.php
+++ resources/views/admin-lte/sidebar.blade.php
@@ -1,64 +1,93 @@
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-light-navy elevation-4">
-    <!-- Brand Logo -->
-    <a href="../../index3.html" class="brand-link">
-      <img src="../../dist/img/AdminLTELogo.png"
-           alt="AdminLTE Logo"
-           class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light">odontoMisiones</span>
-    </a>
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-light-navy elevation-4">
+  <!-- Brand Logo -->
+  <a href="../../index3.html" class="brand-link">
+    <span class="brand-text font-weight-light">odontoMisiones</span>
+  </a>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">Usuario</a>
-        </div>
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar user (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
       </div>
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="../../index.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v1</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="../../index2.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v2</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="../../index3.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v3</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
+      <div class="info">
+        <a href="#" class="d-block">Usuario</a>
+      </div>
     </div>
-    <!-- /.sidebar -->
-  </aside>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+        <li class="nav-item has-treeview {{ (request()->routeIs('clientes.*')) ? 'menu-open active' : '' }}">
+          <a href="#" class="nav-link"
+            style="{{ (request()->routeIs('clientes.*')) ? 'background-color: #3c8dbc; color: white;' : '' }}">
+            <i class="nav-item fas fa-users"></i>
+            <p>
+              Clientes
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="{{route('clientes.index')}}" class="nav-link"
+                style="{{ (request()->routeIs('clientes.index')) ? 'color: #3c8dbc;' : '' }}">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Ver clientes</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item has-treeview {{ (request()->routeIs('equipos.*')) ? 'menu-open active' : '' }}">
+          <a href="#" class="nav-link"
+            style="{{ (request()->routeIs('equipos.*')) ? 'background-color: #3c8dbc; color: white;' : '' }}">
+            <i class="nav-item fas fa-dolly"></i>
+            <p>
+              Equipos
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="{{route('equipos.index')}}" class="nav-link"
+                style="{{ (request()->routeIs('equipos.index')) ? 'color: #3c8dbc;' : '' }}">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Ver equipos</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item has-treeview {{ (request()->routeIs('partes.*') ||request()->routeIs('proveedores.*')) ? 'menu-open active' : '' }}">
+          <a href="#" class="nav-link"
+            style="{{ (request()->routeIs('partes.*') ||request()->routeIs('proveedores.*')) ? 'background-color: #3c8dbc; color: white;' : '' }}">
+            <i class="nav-item fas fa-people-carry"></i>
+            <p>
+              Proveedores y partes
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="{{route('proveedores.index')}}" class="nav-link"
+                style="{{ (request()->routeIs('proveedores.index')) ? 'color: #3c8dbc;' : '' }}">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Ver proveedores</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="{{route('partes.index')}}" class="nav-link"
+                style="{{ (request()->routeIs('partes.index')) ? 'color: #3c8dbc;' : '' }}">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Ver partes</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
