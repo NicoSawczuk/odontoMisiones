bc28cf1069832bd339bbc0511f67a42fa5227981
joaquinpotocki <joaquinpotocki@gmail.com>
2020-07-25 16:46:06 -0300

    Modificacion en el editar
    Modificaciones hechas en la vista editar proveedor

--- resources/views/proveedores/edit.blade.php
+++ resources/views/proveedores/edit.blade.php
@@ -2,20 +2,20 @@
 
 @section('content')
 <form class="form-group " method="POST" action="{{route("proveedores.update",$proveedor->id)}}">
-    @method('PUT')
+    @method("PUT")
     <div class="card card-primary card-outline">
         <div class="card-header">
             <h5>
-                Editar proveedor
+                Modificar proveedor
             </h5>
         </div>
         <div class="card-body">
             <div class="row">
-                <div class="col">
+                <div class="col-4">
                     <div class="form-group ">
                         <label for="empresa">Empresa</label>
                         <input type="text" class="form-control  @error('empresa') is-invalid @enderror" id="empresa"
-                            name="empresa" value="{{ $proveedor->empresa ?? old('empresa') }}" placeholder="Especifique el nombre de su empresa" required>
+                            name="empresa" value="{{ old('empresa') ?? $proveedor->empresa}}" placeholder="Especifique el nombre de su empresa" required>
                         @error('empresa')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
@@ -23,11 +23,63 @@
                         @enderror
                     </div>
                 </div>
-                <div class="col">
+                <div class="col-4">
+                    <div class="form-group ">
+                        <label for="nombre_persona_contacto">Nombre persona contacto</label>
+                        <input type="text" class="form-control  @error('nombre_persona_contacto') is-invalid @enderror" id="nombre_persona_contacto"
+                            name="nombre_persona_contacto" value="{{ old('nombre_persona_contacto') ?? $proveedor->nombre_persona_contacto}}" placeholder="Especifique su nombre de persona de contacto" required>
+                        @error('nombre_persona_contacto')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="form-group ">
+                        <label for="apellido_persona_contacto">Apellido persona contacto</label>
+                        <input type="text" class="form-control  @error('apellido_persona_contacto') is-invalid @enderror" id="apellido_persona_contacto"
+                            name="apellido_persona_contacto" value="{{ old('apellido_persona_contacto') ?? $proveedor->apellido_persona_contacto}}" placeholder="Especifique su apellido de persona de contacto" required>
+                        @error('apellido_persona_contacto')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <div class="form-group ">
+                        <label for="telefono">Telefono</label>
+                        <input type="number" class="form-control  @error('telefono') is-invalid @enderror"
+                            id="telefono" name="telefono" value="{{ old('telefono') ?? $proveedor->telefono}}"
+                            placeholder="Especifique su telefono" required>
+                        @error('telefono')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="form-group ">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control  @error('email') is-invalid @enderror"
+                            id="email" name="email" value="{{ old('email') ?? $proveedor->email}}"
+                            placeholder="Especifique su email" required>
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-4">
                     <div class="form-group ">
                         <label for="direccion_postal">Direccion postal</label>
-                        <input type="text" class="form-control  @error('direccion_postal') is-invalid @enderror" id="direccion_postal"
-                            name="direccion_postal" value="{{ $proveedor->direccion_postal ?? old('direccion_postal') }}" placeholder="Especifique sus direccion postal"
+                        <input type="number" class="form-control  @error('direccion_postal') is-invalid @enderror" id="direccion_postal"
+                            name="direccion_postal" min="0" value="{{ old('direccion_postal') ?? $proveedor->direccion_postal}}" placeholder="Especifique su direccion postal"
                             required>
                         @error('direccion_postal')
                         <span class="invalid-feedback" role="alert">
@@ -38,64 +90,13 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-3">
+                <div class="col-8">
                     <div class="form-group ">
-                        <label for="telefono">Telefono</label>
-                        <input type="" class="form-control  @error('telefono') is-invalid @enderror"
-                            id="telefono" name="telefono" value="{{ $proveedor->telefono ?? old('telefono') }}"
-                            placeholder="Especifique sus telefono" required>
-                        @error('telefono')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
+                        <label for="nombre">Notas generales</label>
+                        <textarea name="notas_generales" id="notas_generales" cols="30" rows="5" class="form-control"
+                            placeholder="Ingrese las notas generales">{{ old('notas_generales') ?? $proveedor->notas_generales}}</textarea>
                     </div>
                 </div>
-                <div class="col-3">
-                    <div class="form-group ">
-                        <label for="email">email</label>
-                        <input type="" class="form-control  @error('email') is-invalid @enderror"
-                            id="email" name="email" value="{{ $proveedor->email ?? old('email') }}"
-                            placeholder="Especifique sus email" required>
-                        @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-3">
-                    <div class="form-group ">
-                        <label for="nombre_persona_contacto">Nombre persona contacto</label>
-                        <input type="nombre_persona_contacto" class="form-control  @error('nombre_persona_contacto') is-invalid @enderror" id="nombre_persona_contacto"
-                            name="nombre_persona_contacto" value="{{ $proveedor->nombre_persona_contacto ?? old('nombre_persona_contacto') }}" placeholder="Especifique su nombre_persona_contacto" required>
-                        @error('nombre_persona_contacto')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                </div>
-                <div class="col-3">
-                    <div class="form-group ">
-                        <label for="apellido_persona_contacto">apellido persona contacto</label>
-                        <input type="apellido_persona_contacto" class="form-control  @error('apellido_persona_contacto') is-invalid @enderror" id="apellido_persona_contacto"
-                            name="apellido_persona_contacto" value="{{ $proveedor->apellido_persona_contacto ?? old('apellido_persona_contacto') }}" placeholder="Especifique su apellido_persona_contacto" required>
-                        @error('apellido_persona_contacto')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-            <div class="form-group ">
-                <label for="nombre">Notas generales</label>
-                <textarea name="notas_generales" id="notas_generales" cols="30" rows="10" class="form-control"
-                    placeholder="Ingrese las notas generales">{{ $proveedor->notas_generales ?? old('notas_generales') }}</textarea>
             </div>
         </div>
         <div class="card-footer float">
