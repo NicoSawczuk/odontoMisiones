7e59ec45a957481433e29589958a7a9605bdcc81
Javier <kachuka99@gmail.com>
2020-07-25 20:36:19 -0300

    creacion de migracion de incidencias

--- database/migrations/2020_07_25_231011_create_incidencias_table.php
+++ database/migrations/2020_07_25_231011_create_incidencias_table.php
@@ -15,6 +15,23 @@ class CreateIncidenciasTable extends Migration
     {
         Schema::create('incidencias', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->date('fecha_comienzo_incidencia');
+            $table->date('fecha_fin_incidencia');
+            $table->double('presupuesto');
+            $table->text('diagnostico_general');
+            $table->double('precio_trabajo');
+            //relacion con el cliente que inicia el incidente
+            $table->unsignedBigInteger('cliente_id');
+            $table->foreign('cliente_id')->references('id')->on('clientes');
+            //relacion con el tecnico encargado del incidente
+            $table->unsignedBigInteger('tecnico_id');
+            $table->foreign('tecnico_id')->references('id')->on('tecnicos');
+            //relacion con el tipo de incidencia
+            $table->unsignedBigInteger('tipo_incidencia_id');
+            $table->foreign('tipo_incidencia_id')->references('id')->on('tipos_incidencias');
+            //relacion con el estado en el que se encuetra la incidencia
+            $table->unsignedBigInteger('estado_id');
+            $table->foreign('estado_id')->references('id')->on('estados');
             $table->timestamps();
         });
     }
