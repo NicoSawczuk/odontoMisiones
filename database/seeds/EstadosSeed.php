82d1ccf17ce8ac7ff56bd1e2c6f6ba25dd1baa29
Javier <kachuka99@gmail.com>
2020-07-28 15:17:38 -0300

    se agrego el color al seed de estados

--- database/seeds/EstadosSeed.php
+++ database/seeds/EstadosSeed.php
@@ -15,27 +15,32 @@ class EstadosSeed extends Seeder
         Estado::create([
             'nombre' => 'sin asignar',
             'descripcion' => 'Estado inicial de cualquier incidencia',
-            'nivel' => 1
+            'nivel' => 1,
+            'color' => '#DC7633'
         ]);
         Estado::create([
             'nombre' => 'asignado',
             'descripcion' => 'Estado que indica que una incidencia se encuentra asignada a un tecnico',
-            'nivel' => 2
+            'nivel' => 2,
+            'color' => '#82E0AA'
         ]);
         Estado::create([
             'nombre' => 'en proceso',
             'descripcion' => 'Estado que indica que se esta tratando la incidencia',
-            'nivel' => 3
+            'nivel' => 3,
+            'color' => '#3498DB'
         ]);
         Estado::create([
             'nombre' => 'pendiente de pago',
             'descripcion' => 'Estado que indica que la incidencia esta terminada pero con el pago pendiente',
-            'nivel' => 4
+            'nivel' => 4,
+            'color' => '#F4D03F'
         ]);
         Estado::create([
             'nombre' => 'pagado',
             'descripcion' => 'Estado que indica que la incidencia ha finalizado',
-            'nivel' => 5
+            'nivel' => 5,
+            'color' => '#4DF827'
         ]);
     }
 }
