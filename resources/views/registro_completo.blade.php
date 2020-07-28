940fff817d98deebeb855855ff28848c5cf62d40
Nicolas Sawczuk <nico.290698@hotmail.com>
2020-07-28 16:48:19 -0300

    Redireccion a mensaje de registro completo

--- resources/views/registro_completo.blade.php
+++ resources/views/registro_completo.blade.php
@@ -17,9 +17,20 @@
         <div class="col-md-9">
             <div class="alert bg-teal alert-dismissible">
                 <h3><i class="fas fa-check-circle"></i> Registro completo</h3>
-                
+
                 <p>Hola {{Auth::user()->name}} {{Auth::user()->apellido}}, Bienvenido a Odonto Misiones
-                <br>Tu perfil está pendiente de revisión, en breve lo estaremos haciendo y ya podrás utilizar nuestros servicios.</p>
+                    <br>Tu perfil está pendiente de revisión, en breve lo estaremos haciendo y ya podrás utilizar
+                    nuestros servicios.
+                    <br>
+                    Por favor esperaa la revisión y
+                    <a href="{{ route('logout') }}"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        vuelve a iniciar sesión
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                </p>
             </div>
         </div>
     </div>
