9a5a957cb346ee236818ee1594ab153c13cbf7c1
Nicolas Sawczuk <nico.290698@hotmail.com>
2020-08-13 10:25:25 -0300

    Vistas de ABM's Pais, Provincia, Localidad

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Cardumen\ArgentinaProvinciasLocalidades\Models\Localidad;
+use Cardumen\ArgentinaProvinciasLocalidades\Models\Pais;
+use Cardumen\ArgentinaProvinciasLocalidades\Models\Provincia;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,5 +27,23 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function indexPais()
+    {
+        $paises = Pais::all();
+        return view('pais_provincia_localidad/paises', compact('paises'));
+    }
+
+    public function indexProvincia()
+    {
+        $provincias = Provincia::all();
+        return view('pais_provincia_localidad/provincias', compact('provincias'));
+    }
+
+    public function indexLocalidades()
+    {
+        $localidades = Localidad::all();
+        return view('pais_provincia_localidad/localidades', compact('localidades'));
     }
 }
